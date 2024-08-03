6caf747a81b3a773874bc26def519f56875e49e3
UkrValik <krasniukevich@gmail.com>
2024-08-03 22:27:41 +0300

    feat: jwt auth strategy

--- awg-backend/apps/account/src/main.ts
+++ awg-backend/apps/account/src/main.ts
@@ -1,22 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.init();
   Logger.log(
-    `🚀 Account is running on: ${port}`
+    `🚀 Account is running`
   );
 }
 
