b4d724395fd35e576a712cc1fbd7440136e3aa5c
UkrValik <krasniukevich@gmail.com>
2024-12-05 10:09:07 +0200

    feat: bottom tab navigation

---
+++ mobile/src/archiv/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+
+import { ScreenWrapper } from '../common/components/screen-wrapper';
+
+export const ArchivScreen = () => {
+  return (
+    <ScreenWrapper>
+      <Text style={{ fontSize: 30 }}>Archiv Screen</Text>
+    </ScreenWrapper>
+  );
+};
